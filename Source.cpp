600496ef67e775724de0f7224bd3f464d9b6a011
Ahmed Mehanna <ahmedabobakr21@hotmail.com>
2021-11-10 06:08:43 +0200

    circle v2, {B, C, D, G, O, P, Q, R, U}

--- Source.cpp
+++ Source.cpp
@@ -26,7 +26,9 @@ struct vertex {
 };
 
 template <typename T>
-void draw(GLenum type, vertex<T>* v, float* rgb, int v_no, bool _3d = false) {
+void draw(GLenum type, vertex<T>* v, float* rgb, int v_no, bool _3d = false, float lw = 5, float ps = 6) {
+    glPointSize(ps);
+    glLineWidth(lw);
     glBegin(type);
     glColor3f(rgb[0], rgb[1], rgb[2]);
     for (int i = 0; i < v_no; i++) {
@@ -37,13 +39,14 @@ void draw(GLenum type, vertex<T>* v, float* rgb, int v_no, bool _3d = false) {
             glVertex3f(v[i].x, v[i].y, v[i].z);
     }
     glEnd();
+    glFlush();
     return;
 }
 
-pair<bool, float> _check(float x, float y, float i, bool right, float _x, float plus) {
-    if (right && x < _x)
+pair<bool, float> _check(float val, float i, bool position, float _val, float plus) {
+    if (position && val < _val)
         return pair<bool, float>(true, i + plus);
-    else if (!right && x > _x)
+    else if (!position && val > _val)
         return pair<bool, float>(true, i + plus);
     //else if (top && y < _y)
     //    return pair<bool, float>(true, i + plus);
@@ -53,39 +56,87 @@ pair<bool, float> _check(float x, float y, float i, bool right, float _x, float 
     return pair<bool, float>(false, i);
 }
 
-void _(float x, float y, float r, float x_sc = 1, float y_sc = 1, bool full = true, bool right = true, float _x = 0) {
+void _(float x, float y, float r, float* rgb, float x_sc = 1, float y_sc = 1, bool full = true, bool _right = true, bool _top = false, bool right = true, bool top = false, float _x = 0, float _y = 0) {
+    glColor3f(rgb[0], rgb[1], rgb[2]);
     float base_x = x;
     float base_y = y;
     cout << base_x << endl;
     pair<bool, float> status;
-    
+
     glBegin(GL_POINTS);
     for (float i = 0; i <= 360;) {
         x = r * cos(i) * x_sc;
-        if (!full)
-            status = _check(x, y, i, right, _x * x_sc, 1);
-        i = status.second;
-        if (status.first)
-            continue;
+        if (!full && _right) {
+            status = _check(x, i, right, _x * x_sc, 1);
+            i = status.second;
+            if (status.first)
+                continue;
+        }
         y = r * sin(i) * y_sc;
-        glVertex2d(x, y);
+        if (!full && _top) {
+            status = _check(y, i, right, _x * x_sc, 1);
+            i = status.second;
+            if (status.first)
+                continue;
+        }
+        glVertex2d(x + base_x, y + base_y);
         cout << x << "  " << _x << endl;
 
         i += .5;
 
         x = r * cos(i) * x_sc;
-        if (!full)
-            status = _check(x, y, i, right, _x * x_sc, .5);
-        i = status.second;
-        if (status.first)
-            continue;
+        if (!full && _right) {
+            status = _check(x, i, right, _x * x_sc, .5);
+            i = status.second;
+            if (status.first)
+                continue;
+        }
         y = r * sin(i) * y_sc;
-        glVertex2d(x, y);
+        if (!full && _top) {
+            status = _check(y, i, right, _x * x_sc, .5);
+            i = status.second;
+            if (status.first)
+                continue;
+        }
+        glVertex2d(x + base_x, y + base_y);
         i += .5;
         cout << x << "  " << _x << endl;
     }
     glEnd();
 }
+
+//void _(float x, float y, float r, float* rgb, float x_sc = 1, float y_sc = 1, bool full = true, bool right = true, float _x = 0) {
+//    glColor3f(rgb[0], rgb[1], rgb[2]);
+//    pair<bool, float> status;
+//    glBegin(GL_POINTS);
+//    glVertex2f(x, y);
+//    for (float i = 0; i <= 360;) {
+//        x = r * cos(i) * x_sc;
+//        cout << x << "  " << _x << endl;
+//        if (!full)
+//            status = _check(x, y, i, right, _x * x_sc, 1);
+//        i = status.second;
+//        if (status.first)
+//            continue;
+//        y = r * sin(i) * y_sc + y;
+//        glVertex2f(x, y);
+//        cout << x << "  " << _x << endl;
+//
+//        i += .5;
+//
+//        x = r * cos(i) * x_sc;
+//        if (!full)
+//            status = _check(x, y, i, right, _x * x_sc, .5);
+//        i = status.second;
+//        if (status.first)
+//            continue;
+//        y = r * sin(i) * y_sc + y;
+//        glVertex2f(x, y);
+//        i += .5;
+//        cout << x << "  " << _x << endl;
+//    }
+//    glEnd();
+//}
 
 void A() {
     float A_co[] = { 1, 0, 0 };
@@ -101,6 +152,39 @@ void A() {
     };
     draw(GL_LINES, A_ve2, A_co2, 2);
 }
+
+void B() {
+    float B_co[] = { 1, 0, 0 };
+    vertex<float> B_ve[] = { vertex<float>(-.3, 0),
+        vertex<float>(-.3, .6)
+    };
+    draw(GL_LINES, B_ve, B_co, 2);
+
+    float B_co2[] = { 1, 0, 0 };
+    _(-.3, .45, .15, B_co2, 1, 1, false, true, 0);
+    
+    float B_co3[] = { 1, 0, 0 };
+    _(-.3, .15, .15, B_co3, 1.15, 1, false, true, 0);
+}
+
+void C() {
+    float C_co[] = { 1, 0, 0 };
+    //_(-.3, .3, .25, C_co, .7, 1, false, false, .1);
+    _(-.3, .3, .25, C_co, .7, 1, false, true, false, false, false, .1);
+    //_(0, 0, .25, C_co, .8, 1.35, false, false, 0);
+}
+
+void D() {
+    float D_co[] = { 1, 0, 0 };
+    vertex<float> D_ve[] = { vertex<float>(-.3, 0),
+        vertex<float>(-.3, .6)
+    };
+    draw(GL_LINES, D_ve, D_co, 2);
+
+    float D_co2[] = { 1, 0, 0 };
+    _(-.3, .3, .15, D_co2, 1, 2, false, true, false, true);
+}
+
 
 void E()
 {
@@ -136,6 +220,20 @@ void F()
     draw(GL_LINES, F_ve2, F_co2, 2);
 
 }
+
+void G() {
+    float G_co[] = { 1, 0, 0 };
+    _(-.3, .3, .25, G_co, .5, .9, false, true, false, false, false, .05);
+
+    float G_co2[] = { 1, 0, 0 };
+    vertex<float> G_ve[] = { vertex<float>(-.3, .075),
+        vertex<float>(-.25, .075),
+        vertex<float>(-.25, .25),
+        vertex<float>(-.3, .25)
+    };
+    draw(GL_LINE_STRIP, G_ve, G_co2, 4);
+}
+
 
 void H()
 {
@@ -234,6 +332,55 @@ void N()
 
 }
 
+void O() {
+    float O_co[] = { 1, 0, 0 };
+    _(-.3, .3, .25, O_co, .65);
+}
+
+void P() {
+    float P_co[] = { 1, 0, 0 };
+    vertex<float> P_ve[] = { vertex<float>(-.3, 0),
+        vertex<float>(-.3, .6)
+    };
+    draw(GL_LINES, P_ve, P_co, 2);
+
+    float P_co2[] = { 1, 0, 0 };
+    _(-.3, .45, .15, P_co2, 1, 1, false, true, false, true);
+}
+
+void Q() {
+    float O_co[] = { 1, 0, 0 };
+    _(-.3, .3, .25, O_co, .65);
+
+    float Q_co[] = { 1, 0, 0 };
+    vertex<float> Q_ve[] = { vertex<float>(-.3, .3),
+        vertex<float>(-.2, -.05)
+    };
+    draw(GL_LINES, Q_ve, Q_co, 2, false, 7);
+}
+
+void R() {
+    float R_co[] = { 1, 0, 0 };
+    vertex<float> R_ve[] = { vertex<float>(-.3, 0),
+        vertex<float>(-.3, .6),
+        vertex<float>(-.3, .58),
+        vertex<float>(-.25, .58),
+        vertex<float>(-.3, .3),
+        vertex<float>(-.25, .3)
+    };
+    draw(GL_LINES, R_ve, R_co, 6, 7);
+
+    float R_co2[] = { 1, 0, 0 };
+    _(-.295, .445, .15, R_co2, .8, 1, false, true, false, true, false, .05);
+
+    float R_co3[] = { 1, 0, 0 };
+    vertex<float> R_ve3[] = { vertex<float>(-.25, .3),
+        vertex<float>(-.2, .1)
+    };
+    draw(GL_LINES, R_ve3, R_co3, 2);
+
+}
+
 void T()
 {
     float T_co[] = { 1, 0, 0 };
@@ -247,6 +394,19 @@ void T()
         vertex<float>(.25, -.5)
     };
     draw(GL_LINES, T_ve2, T_co2, 2);
+}
+
+void U() {
+    float U_co[] = { 1, 0, 0 };
+    _(-.3, .3, .25, U_co, .5, .9, false, false, true, false, true, .05);
+
+    float U_co2[] = { 1, 0, 0 };
+    vertex<float> U_ve2[] = { vertex<float>(-.426, .3),
+        vertex<float>(-.426, .65),
+        vertex<float>(-.177, .3),
+        vertex<float>(-.177, .65)
+    };
+    draw(GL_LINES, U_ve2, U_co2, 4, 7);
 }
 
 void V()
@@ -339,14 +499,17 @@ void display() {
         GL_POLYGON (any no. of points)
     */
 
-    A();
+    Q();
     //glColor3f(0, 1, 0);
     //glBegin(GL_POINTS);
     //glVertex2f(0, 0);
     //glEnd();
 
-    glColor3f(1, 0, 0);
-    _(1, 1, .5, .5, 1, false, true, -.15);
+    //glColor3f(1, 0, 0);
+    //float x[]{ 1, 0, 0 };
+    // _(-.3, .3, .25, x, 1, 1, false, true, true, false, false, .05, .05);
+    //_(-.9, 0, .5);
+    //_(0, 0, .5, x, .5, 1, false, true, -.15);
     //_circle2();
 
     glutSwapBuffers();  //  in Double Buffers => glutSwapBuffers(hDC)
@@ -391,3 +554,4 @@ int main(int argc, char** argv) {
     //glutSpecialFunc(specialKeys);
     glutMainLoop(); // To keep the window running
 
+}
